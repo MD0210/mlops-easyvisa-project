0d10f8f80825a0bf8e2c93f57d2de618a306b598
Marc Daniel Ortiz <marcdaniel.ortiz@gmail.com>
2025-07-16 04:17:21 +0800

    update on setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="us_visa",
-    version="0.0.0",
-    author="marcdanielortiz",
-    author_email="marcdaniel.ortiz@gmail.com",
-    packages= find_packages()
-)
+    name = "us_visa",
+    version = "0.0.0",
+    author = "marcdanielortiz",
+    author_email = "marcdaniel.ortiz@gmail.com",
+    packages = find_packages()
