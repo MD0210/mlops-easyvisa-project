1cfed9d08cbddb0e7e497a6133118bff2a46c5e4
Marc Daniel Ortiz <marcdanie.ortiz@gmail.com>
2025-07-24 02:01:55 +0800

    data transformation coding include mapping in entity

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -18,6 +18,8 @@ CURRENT_YEAR = date.today().year
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessor.pkl"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
+
+
 """
 Data Ingestion related constants start with DATA_INGESTION VAR NAME
 """
@@ -35,3 +37,11 @@ Data Validation related constants start with DATA_VALIDATION VAR NAME
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "data_drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+"""
+Data Transformation related constants start with DATA_TRANSFORMATION VAR NAME
+"""
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
